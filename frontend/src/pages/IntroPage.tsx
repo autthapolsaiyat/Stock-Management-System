74171f0050398166438a8ee6f26830c928023dc5
Autthapol Saiyat <saiyat2009@gmail.com>
2025-12-21 09:38:04 +0700

    fix: Add SafetyOutlined import

--- frontend/src/pages/IntroPage.tsx
+++ frontend/src/pages/IntroPage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Avatar, Button, Modal, QRCode, Space, Switch, message } from 'antd';
 import { 
-  UserOutlined, 
+  UserOutlined, SafetyOutlined, 
   EnvironmentOutlined, 
   FileTextOutlined,
   ShareAltOutlined,
